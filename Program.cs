8b88ecdef08372fd4dd032ba19b74978c9844c70
Sarmen777 <bagmanyansarmen0@gmail.com>
2025-03-01 22:00:10 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,161 +1,105 @@
 using System;
 /*
-class BankAccount
- {
-    public int AccountNumber{get; private set;}
-    public string CustomerName{get; private set;}
-    public decimal Balance {get; private set;}
-
-    public BankAccount(int accountNumber, string customerName, decimal balance = 0)
-    {
-        AccountNumber = accountNumber;
-        CustomerName = customerName;
-        Balance = balance;
-    }
-
-     public override string ToString()
-    {
-         return $"Customer name: {CustomerName} Account number: {AccountNumber} Balance: {Balance}$ ";
-     }
-     
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is BankAccount other)
-        {
-            return this.AccountNumber == other.AccountNumber;
-        }
-        return false;
-     }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(this.AccountNumber, this.Balance, this.CustomerName);
-    }
-
-    public static BankAccount operator +(BankAccount account1, int deposit)
-    {
-        account1.Balance += deposit;
-        return account1;
-    }
-
-    public static BankAccount operator -(BankAccount account1, int withdraw)
-    {
-        if(account1.Balance < withdraw)
-         {
-             System.Console.WriteLine("Balance can not be negative");
-             return account1;
-         }
-         account1.Balance -= withdraw;
-         return account1;
-     }
-
-     public static bool operator > (BankAccount account1, BankAccount account2)
-     {
-         return account1.Balance > account2.Balance;
-     }
-
-     public static bool operator < (BankAccount account1, BankAccount account2)
-     {
-         return account1.Balance < account2.Balance;
-     }
-
-     public static bool operator >= (BankAccount account1, BankAccount account2)
-     {
-         return account1.Balance >= account2.Balance;
-     }
-
-     public static bool operator <= (BankAccount account1, BankAccount account2)
-     {
-         return account1.Balance <= account2.Balance;
-     }
-
-
- }
- class Program
- {
-     static void Main()
-     {
-         BankAccount account = new BankAccount(1, "John");
-         account += 2400;
-         System.Console.WriteLine(account);
-     }
- }
-*/
-class Vector3D
+class WaterTank
 {
-    public int X {get; private set;}
-    public int Y {get; private set;}
-    public int Z {get; private set;}
+     private double Capacity = 10000;
+     private double CurrentLevel;
 
-    public Vector3D(int x, int y, int z)
+    public WaterTank()
     {
-        X = z;
-        Y = y;
-        Z = z;
+        CurrentLevel = 0;
+    }
+    public WaterTank(double CurrentLevel)
+    {
+        this.CurrentLevel = CurrentLevel;    
+    }
+    public override string ToString() 
+    {
+        return $"Capacity: {Capacity}, CurrentLevel: {CurrentLevel}";
+    }
+    public static void Transfer(WaterTank obj1 ,WaterTank obj2 ,double amount)
+    {
+        if(obj1.CurrentLevel < amount || obj2.CurrentLevel + amount > obj2.Capacity)
+        {
+            Console.WriteLine("Can not do this , input another amount");
+            return;
+        }
+        obj1.CurrentLevel -= amount;
+        obj2.CurrentLevel += amount;
+    }
+
+    public static WaterTank operator +(WaterTank obj, double amount)
+    {
+        if (obj.CurrentLevel + amount > obj.Capacity)
+        {
+            Console.WriteLine("Not enough capacity");
+            return obj;
+        }
+        return new WaterTank(obj.CurrentLevel + amount);
+    }
+
+    public static WaterTank operator -(WaterTank obj, double amount)
+    {
+        if (obj.CurrentLevel - amount < obj.Capacity)
+        {
+            Console.WriteLine("Not enough water");
+            return obj;
+        }
+        return new WaterTank(obj.CurrentLevel - amount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        WaterTank obj1 = new WaterTank();
+        WaterTank obj2 = new WaterTank();
+        obj1 = obj1 + 3000;
+        obj2 = obj2 + 5000;
+        WaterTank.Transfer(obj1, obj2, 2000);
+        Console.WriteLine(obj1);
+        Console.WriteLine(obj2);
+    }
+}
+*/
+class InkReservoir
+{
+    public string Color {get; private set;}
+    public double InkAmount {get; private set;}
+
+    public InkReservoir(string color, double inkAmount)
+    {
+        Color = color;
+        InkAmount = inkAmount;
+    }
+
+    public static InkReservoir operator +(InkReservoir a, InkReservoir b)
+    {
+        if (a.Color == b.Color)
+        {
+            return new InkReservoir(a.Color, a.InkAmount + b.InkAmount);
+        }
+        else
+        {
+            Console.WriteLine("Can not combine different colours");
+            return a;
+        }
+    }
+
+    public static InkReservoir operator -(InkReservoir reservoir, double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Can not do this , input digit higher than 0");
+            return reservoir;
+        }
+        return new InkReservoir(reservoir.Color, reservoir.InkAmount - amount);
     }
 
     public override string ToString()
     {
-        return $"X: {X}, Y: {Y}, Z: {Z}";
+        return $"Color: {Color}, Amount: {InkAmount}";
     }
-
-    public static Vector3D operator +(Vector3D vec1, Vector3D vec2)
-    {
-        return new Vector3D(vec1.X + vec2.X, vec1.Y + vec2.Y,vec1.Z + vec2.Z);
-    }
-
-    public static Vector3D operator -(Vector3D vec1, Vector3D vec2)
-    {
-        return new Vector3D(vec1.X - vec2.X, vec1.Y - vec2.Y,vec1.Z - vec2.Z);
-    }
-
-    public static Vector3D operator *(Vector3D vec1, Vector3D vec2)
-    {
-        return new Vector3D(vec1.X * vec2.X, vec1.Y * vec2.Y,vec1.Z * vec2.Z);
-    }
-
-    public static Vector3D operator /(Vector3D vec1, Vector3D vec2)
-    {
-        if (vec2.X == 0 || vec2.Y == 0 || vec2.Z == 0)
-        {
-            System.Console.WriteLine("Cant divide by 0 , returning vector by default coordiantes");
-            return new Vector3D(0,0,0);
-        }
-        return new Vector3D(vec1.X - vec2.X, vec1.Y - vec2.Y,vec1.Z - vec2.Z);
-    }
-
-    public static bool operator ==(Vector3D vec1, Vector3D vec2)
-    {
-        return vec2.X == vec1.X && vec2.Y == vec1.Y && vec2.Z == vec1.Z;
-    }
-    public static bool operator !=(Vector3D vec1, Vector3D vec2)
-    {
-        return vec2.X != vec1.X || vec2.Y != vec1.Y || vec2.Z != vec1.Z;
-    }
-    public static bool operator true(Vector3D vec1)
-    {
-        return vec1.X != 0 && vec1.Y != 0 && vec1.Z !=0;
-    }
-
-    public static bool operator false(Vector3D vec1)
-    {
-        return vec1.X == 0 || vec1.Y == 0 || vec1.Z ==0;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is Vector3D other)
-        {
-            return (Vector3D)obj == other;
-        }
-        return false;
-    }
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(X,Y,Z);
-    }
-
 }
-    
 
