98b078536b411462133bf5b6d6cbde16d27bf5b3
Sarmen777 <bagmanyansarmen0@gmail.com>
2025-04-15 11:49:01 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,118 +1,277 @@
 using System;
+using System.Collections.Generic;
 
-class Parent
+/*1*************************************************************/
+public delegate double Operation(double a, double b);
+public static class Calculator
 {
-    public string Name { get; private set}
-    public int Age {get; private set}
-
-    public int Salary {get; private set}
-
-    public Parent (string name , int age , int salary)
+    public static double Add(double a, double b) => a + b;
+    public static double Subtract(double a, double b) => a - b;
+    public static double Multiply(double a, double b) => a * b;
+    public static double Divide(double a, double b)
     {
-        Name = name;
-        Age = age;
-        Salary = salary;
-    }
-    public override string ToString ()
-    {
-        return $"Name: {Name} , Age: {Age} , Salary: {Salary}";
-    }
+        if (b == 0) throw new ArgumentException("Can not divide by 0");
 
+        return a / b;
+    }
+} // Operation op = add , sub and using switch cases for choice
+
+/*2*****************************************************************************/
+public delegate void Notify (string msg);
+public static class Notifications
+{
+    public static void ConsoleNotification (string msg) => Console.WriteLine($"Console: {msg}");
+    public static void EmailNotification (string msg) => Console.WriteLine($"Email: {msg}");
 }
-class Child
+public class Notifier
 {
-    public string Name { get; private set;}
-    public int Age {get; private set;}
+    Notify? notify;
 
-    public Parent Mother;
-    public Parent Father;
-
-    public Child (string name , int age , Parent mother , Parent father)
+    public void SendMessage(string msg)
     {
-        Name = name;
-
-        Age = age;
-
-        Mother = mother;
-
-        Father = father;
-    }
-    public override string ToString ()
-    {
-        return $"{Name} , Age: {Age} , Mother: {Mother} , Father: {Father}";
-    }
-    public static Child[] childs = new Child[]
-    {
-        new Child("Tigran", 13, new Parent("Anna", 32, 270000), new Parent("Arman", 44, 500000)),
-        new Child("Lusine", 5, new Parent("Mari", 28, 300000), new Parent("Hakob", 38, 450000)),
-        new Child("Gor", 10, new Parent("Lusine", 34, 250000), new Parent("Vahan", 42, 600000)),
-        new Child("Ara", 8, new Parent("Sona", 30, 350000), new Parent("Vardan", 40, 400000)),
-        new Child("Grigor", 12, new Parent("Vera", 29, 320000), new Parent("Ashot", 45, 480000)),
-    };
-
-    public static void PrintWithAgeLess70()
-    {
-        foreach (Child child in childs)
+        if (notify != null)
         {
-            if (child.Father.Age + child.Mother.Age < 70) Console.WriteLine(child);
+            notify(msg);
         }
-    }
-    public static int FindSalaryOfOldest()
-    {
-        Parent fatherOfOldest = null;
-        int maxIndex = 0;
-
-        foreach (Child child in childs)
+        else
         {
-            if (child.Age > maxIndex)
-            {
-                maxIndex = child.Age;
-                fatherOfOldest = child.Father;
-            }
+            Console.WriteLine("There are no methods");
         }
-        return fatherOfOldest.Salary;
-    }
-    public static void FindChildWithHigestIncome()
-    {
-        int maxIncome = 0;
-        Child richestChild = null;
-
-        foreach (Child child in childs)
-        {
-            if (child.Father.Salary + child.Mother.Salary > maxIncome)
-            {
-                maxIncome = child.Father.Salary + child.Mother.Salary;
-                richestChild = child;
-            }
-        }
-        Console.WriteLine(richestChild);
-    }
-    public static void SwapOldestAndYoungest()
-    {
-        int youngestIndex = 0;
-        int oldestIndex = 0;
-
-        for (int i = 1; i < childs.Length; i++)
-        {
-         Child currentChild = childs[i];
-
-         if (currentChild.Age < childs[youngestIndex].Age) youngestIndex = i;
-             
-         if (currentChild.Age > childs[oldestIndex].Age)   oldestIndex = i; 
-
-        }
-
-        Child temp = childs[youngestIndex];
-        childs[youngestIndex] = childs[oldestIndex];
-        childs[oldestIndex] = temp;
-
-        foreach (Child child in childs)
-         {
-          Console.WriteLine(child);
-         }
     }
 }
 
+/*3*****************************************************************************/
+
+public delegate T Transformer<T>(T input);
+
+public static class ListTransformer
+{
+    public static List<T> TransformList<T>(this List<T> list, Transformer<T> transformer)
+    {
+        List<T> result = new List<T>();
+        foreach (T item in list)
+        {
+            result.Add(transformer(item));
+        }
+        return result;
+    }
+}
+public static class Methods
+{
+public static string ToUpperCase(string input)
+{
+    return input.ToUpper();
+}
+
+}
+class Program
+{
+    public static void Main()
+    {
+        Transformer<string> transformer = Methods.ToUpperCase;
+
+        List<string> list = new List<string>();
+        list.Add("something");
+        list.Add("inchka");
+
+        List<string> transformed = list.TransformList(transformer);
+
+        foreach (var i in transformed)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+/*4******************************/
+
+public static class MyListExtensions
+{
+    public static List<T> Filter<T>(this List<T> list, Predicate<T> predicate)
+    {
+        List<T> result = new List<T>();
+        foreach (T item in list)
+        {
+            if (predicate(item))
+            {
+                result.Add(item);
+            }
+        }
+        return result;
+    }
+}
+public static class MyMethods
+{
+    public static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+}
+
+/*5*****************************************************************************/
+
+public class Person
+{
+    public string? Name { get; set;}
+    public int Age { get; set; }
+}
+
+public static class PersonSorter
+{
+    public static void SortByAgeAscending(List<Person> people)
+    {
+        people.Sort((item1, item2) => item1.Age.CompareTo(item2.Age));
+    }
+
+    public static void SortByNameAlphabetically(List<Person> people)
+    {
+        people.Sort((item1, item2) => item1.Name.CompareTo(item2.Name));
+    }
+}
+
+
+/*First Task************************************/
+
+namespace second{
+public class Shop
+{
+    public string? Name { get; set;}
+    private int _discount;
+    public int Discount
+    {
+        get { return _discount; }
+
+        set {
+            if (value != _discount)
+            {
+                int old = _discount;
+                _discount = value;
+                OnDiscountChanged(old , _discount);
+
+            }
+        }
+    }
+    public Shop(string name)
+    {
+        Name = name;
+    }
+
+    public event EventHandler<DiscountChangedEventArgs> DiscountChanged;
+
+    protected virtual void OnDiscountChanged(int oldPrice , int newPrice)
+    {
+        DiscountChanged?.Invoke(this, new DiscountChangedEventArgs(oldPrice , newPrice));
+    }
+
+}
+public class DiscountChangedEventArgs : EventArgs
+{
+    public int OldDiscount;
+    public int NewDiscount;
+
+    public DiscountChangedEventArgs (int oldPrice, int newPrice)
+    {
+        OldDiscount = oldPrice;
+        NewDiscount = newPrice;
+    }
+
+}
+public class Customer
+{
+    public string Name { get; }
+
+    public Customer(string name)
+    {
+        Name = name;
+    }
+
+    public void Subscribe(Shop shop)
+    {
+        shop.DiscountChanged += RespondToDiscountChange;
+    }
+
+    private void RespondToDiscountChange(object sender, DiscountChangedEventArgs e)
+    {
+        Shop? shop = sender as Shop;
+        if (shop != null)
+        {
+            Console.WriteLine($"[{Name}]-ն ասում է, որ [{shop.Name}] խանութում զեղչը բարձրացել է {e.OldDiscount}%֊ից մինչև {e.NewDiscount}%։");
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Shop shop = new Shop("TechShop");
+        Customer alice = new Customer("Alice");
+        Customer bob = new Customer("Bob");
+
+        alice.Subscribe(shop);
+        bob.Subscribe(shop);
+
+        shop.Discount = 10;
+        shop.Discount = 20;
+    }
+}
+}
+
+/*Task2*************************************************************/
+
+namespace t2{
+public interface IReadBox<out T>
+{
+    T this[int index] { get; }
+}
+
+public interface IWriteBox<in T>
+{
+    void Add(T item);
+}
+
+public class MyList<T> : IReadBox<T>, IWriteBox<T>
+{
+    private List<T> _list = new List<T>();
+
+    public T this[int index]
+    {
+        get { return _list[index]; }
+    }
+
+    public void Add(T item)
+    {
+        _list.Add(item);
+    }
+}
+public static class Test
+{
+    public static void ReadOnlyTest(IReadBox<object> box)
+    {
+        Console.WriteLine("Reading first element as object: " + box[0]);
+    }
+
+    public static void WriteOnlyTest(IWriteBox<string> box)
+    {
+        box.Add("Hello");
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        MyList<string> stringList = new MyList<string>();
+        stringList.Add("Hello");
+        stringList.Add("World");
+
+        Test.ReadOnlyTest(stringList);
+
+        MyList<object> objectList = new MyList<object>();
+        Test.WriteOnlyTest(objectList);
+
+        Console.WriteLine("objectList[0]: " + objectList[0]);
+    }
+}
+}
 
 
 
